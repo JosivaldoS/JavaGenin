ec74dc2ad348795d54c95b06bc5738de0c47a121
Josivaldo Soares <josivaldojr365@gmail.com>
2025-10-04 20:09:51 -0300

    Estudando sobre tipos não primitivos
    Entendendo sobre metódos e tipos não primitivos

--- src/TiposPrimitivos/DadosNaoPrimitivos.java
+++ src/TiposPrimitivos/DadosNaoPrimitivos.java
@@ -4,7 +4,25 @@ public class DadosNaoPrimitivos {
 
     static void main(String[] args) {
 
-        // D
+        /*
+        * Dados não primitivos: String, Array, Class, enum
+        * Objetivo: Criar um ninja, e atribuir métodos a ele.
+         */
+
+        String nome = "Kakashi Hatake";
+        String nomeEmCaixaAlta = nome.toUpperCase(); // Irá transformar tudo em CAPSLOCK
+        System.out.println("Nome em maiúscula: " + nomeEmCaixaAlta);
+        System.out.println("Nome: " + nome);
+
+        String testeMetodos = nome.intern();
+
+        String aldeia = "Aldeia da folha";
+        String aldeiaEmCaixaBaixa= aldeia.toLowerCase();
+
+        System.out.println("Aldeia: " + aldeia);
+        System.out.println("Aldeia Em Caixa Baixa: " + aldeiaEmCaixaBaixa);
+
+        System.out.println(testeMetodos);
 
     }
 
