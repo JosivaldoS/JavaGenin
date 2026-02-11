dd89daaac4038c397c2c3fea5209a03db2b644a1
JosivaldoS <josivaldojr365@gmail.com>
2026-02-11 20:12:50 -0300

    Estudando Sobre Arryays e Laços de repetição

---
+++ src/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,28 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    static void main(String[] args) {
+
+        /*
+         Possuimos duas formas de usar um laço de repetição
+         while e for
+         */
+
+        // While
+
+        int numeroDeClones = 0;
+        int numeroMaxDeClones = 20;
+
+        while (numeroDeClones <= numeroMaxDeClones) {
+            System.out.println("Kage Bunshin no Jutsu! Numero de clones: " + numeroDeClones);
+            numeroDeClones += 1;
+        }
+
+        // For
+
+        for (int i = 0; i <= numeroMaxDeClones; i++) {
+            System.out.println("Kage Bunshin do Naruto no Jutsu! Numero de clones: " + i);
+        }
+
+    }
+}
---
+++ src/NivelBasico/Array.java
@@ -0,0 +1,25 @@
+package NivelBasico;
+
+import java.util.Arrays;
+
+public class Array {
+
+    static void main() {
+        /* Como conseguimos utilizar um sout de um array
+        / para imprimirmos todo o array
+        e pq aparece aquele erro ao utilizarmos um sout em um array
+        [Ljava.lang.String;@54bedef2
+        [Ljava.lang.String;@54bedef2
+        */
+
+        String[] ninjas = new String[4];
+        ninjas[0] = "Kakashi Hatake";
+        ninjas[1] = "Obito Uchiha";
+        ninjas[2] = "Rin Nohara";
+        ninjas[3] = "Minato Uzumaki";
+
+        System.out.println(Arrays.toString(ninjas));
+
+    }
+
+}
